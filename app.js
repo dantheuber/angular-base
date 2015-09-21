c87e6a667d6d75767b632272ebf357b3d6c26252
Daniel Essig <dessig@godaddy.com>
2015-09-21 13:54:11 -0700

    started html and services

--- app.js
+++ app.js
@@ -3,14 +3,13 @@
 require('angular');
 require('angular-route');
 require('angular-query-string');
-require('ngSmoothScroll');
 require('angular-sanitize');
 
 var app = require('angular').module('baseAngularApp', ['ngRoute', 'angular-query-string', 'ngSanitize']);
 
 require('./lib/js/init')(app);
-require('./lib/js/routes')();
-require('./lib/js/controllers/index')();
-require('./lib/js/directives/index')();
-require('./lib/js/filters/index')();
-require('./lib/js/services/index')();
+require('./lib/js/routes')(app);
+require('./lib/js/controllers')(app);
+require('./lib/js/directives')(app);
+require('./lib/js/filters')(app);
+require('./lib/js/services')(app);
