bb2c76dd95abd8251f87304a31c7efc45c5a0e64
Daniel Essig <dessig@godaddy.com>
2015-09-26 11:44:30 -0700

    added readme, removed materialize to have bundled properly

---
+++ README.md
@@ -0,0 +1,20 @@
+#angular-base
+A simple project from which angular projects can be started.
+
+##Install Dependencies
+This project uses `gulp` and `jspm`, as well as `compass` to make your development life easier. So you will need to install the necessary dependencies via the following commands:
+```shell
+#Install global commands used by gulp
+sudo npm install -g gulp jspm jspm-bower-endpoint
+#Setup jspm bower endpoint
+jspm registry create bower jspm-bower-endpoint
+#Install local node modules
+npm install
+```
+
+
+##Start Working
+Run `gulp host` to bundle the application and host it on `localhost:5003`
+
+
+###This readme, very incomplete =)
--- config.js
+++ config.js
@@ -11,6 +11,7 @@ System.config({
 System.config({
   "map": {
     "angular": "npm:angular@1.4.6",
+    "angular-material": "bower:angular-material@0.11.1",
     "angular-query-string": "bower:angular-query-string@1.0.1",
     "angular-route": "npm:angular-route@1.4.6",
     "angular-sanitize": "npm:angular-sanitize@1.4.6",
@@ -18,6 +19,18 @@ System.config({
     "ngSmoothScroll": "github:d-oliveros/ngSmoothScroll@1.7.1",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
+    "bower:angular-animate@1.4.6": {
+      "angular": "bower:angular@1.4.6"
+    },
+    "bower:angular-aria@1.4.6": {
+      "angular": "bower:angular@1.4.6"
+    },
+    "bower:angular-material@0.11.1": {
+      "angular": "bower:angular@1.4.6",
+      "angular-animate": "bower:angular-animate@1.4.6",
+      "angular-aria": "bower:angular-aria@1.4.6",
+      "css": "github:systemjs/plugin-css@0.1.18"
+    },
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
